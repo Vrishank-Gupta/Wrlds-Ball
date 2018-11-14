3b72ba96cbb23163bffd6631953b96bf43573c28
Vrishank <vrishank98@gmail.com>
2018-11-14 23:02:37 +0530

    Name AfterBounce
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- app/src/main/java/com/example/vrishankgupta/boilerplate/AfterBounce.java
+++ app/src/main/java/com/example/vrishankgupta/boilerplate/AfterBounce.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.wrlds.sdk.Ball;
@@ -16,16 +17,23 @@ import mehdi.sakout.fancybuttons.FancyButton;
 public class AfterBounce extends AppCompatActivity {
 
     TextView tv;
-    FancyButton fancyButton,counter;
+    EditText etName;
+    FancyButton fancyButton,counter,btnName;
+    static public String name = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_bounce);
         tv = findViewById(R.id.forceValue);
+        etName = findViewById(R.id.etName);
+        btnName = findViewById(R.id.btnName);
+        btnName.setText("Register");
+        btnName.setRadius(55);
+        btnName.setTextSize(20);
         counter = findViewById(R.id.counter);
         counter.setText("No. of bounces:- " + BeforeBounce.count);
         counter.setRadius(55);
-        counter.setTextSize(30);
+        counter.setTextSize(25);
         fancyButton = findViewById(R.id.fancy);
         fancyButton.setText("Reset");
         fancyButton.setRadius(55);
@@ -37,8 +45,27 @@ public class AfterBounce extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 BeforeBounce.count = 0;
+                name = "";
                 startActivity(new Intent(AfterBounce.this,BeforeBounce.class));
                 finish();
+            }
+        });
+
+        btnName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etName.getText().toString().equals(null) || etName.getText().toString().equals(""))
+                {
+                    etName.setError("Empty Field");
+                }
+
+                else {
+                    counter.setText("No. of bounces by " + etName.getText().toString() + ":- " + BeforeBounce.count);
+                    name = etName.getText().toString();
+                    counter.setTextSize(25);
+                }
+
+
             }
         });
 
--- app/src/main/java/com/example/vrishankgupta/boilerplate/BeforeBounce.java
+++ app/src/main/java/com/example/vrishankgupta/boilerplate/BeforeBounce.java
@@ -16,7 +16,7 @@ import com.wrlds.sdk.Ball;
 public class BeforeBounce extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
     static int count = 0;
 
-//    Ball a;
+    Ball a;
     ImageView img;
     public static final String API_KEY = "AIzaSyCE7i9MJ3YuDuqy9cn4YqgSf-qDESbgXh4";
     public static final String VIDEOID = "aA_mqSzbkM0";
@@ -26,30 +26,30 @@ public class BeforeBounce extends YouTubeBaseActivity implements YouTubePlayer.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_before);
         img = findViewById(R.id.img);
-//        a=MainActivity.a;
+        a=MainActivity.a;
         YouTubePlayerView youTubePlayerView = findViewById(R.id.ytPlayer);
         youTubePlayerView.initialize(API_KEY,this);
 
 
-//        a.setOnShakeListener(new Ball.OnShakeListener() {
-//            @Override
-//            public void onShake(float v) {
-//                Toast.makeText(a, "Shaken"  + v, Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        a.setOnBounceListener(new Ball.OnBounceListener() {
-//            @Override
-//            public void onBounce(int i, float v) {
-//                Log.d("BounceType", Ball.BounceType.HARD + "");
-//                String str = String.valueOf(v);
-//                if(v > 10)
-//                {
-//                    count++;
-//                    startActivity(new Intent(BeforeBounce.this,AfterBounce.class).putExtra("force",str).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-//                    finish();
-//                }
-//            }
-//        });
+        a.setOnShakeListener(new Ball.OnShakeListener() {
+            @Override
+            public void onShake(float v) {
+                Toast.makeText(a, "Shaken"  + v, Toast.LENGTH_SHORT).show();
+            }
+        });
+        a.setOnBounceListener(new Ball.OnBounceListener() {
+            @Override
+            public void onBounce(int i, float v) {
+                Log.d("BounceType", Ball.BounceType.HARD + "");
+                String str = String.valueOf(v);
+                if(v > 10)
+                {
+                    count++;
+                    startActivity(new Intent(BeforeBounce.this,AfterBounce.class).putExtra("force",str).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                    finish();
+                }
+            }
+        });
 
     }
 

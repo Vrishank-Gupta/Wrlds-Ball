2ca9f92964486ea5879194a88d003cb1532d7db2
Vrishank <vrishank98@gmail.com>
2018-11-14 11:22:31 +0530

    Initial Name
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- app/src/main/java/com/example/vrishankgupta/boilerplate/BeforeBounce.java
+++ app/src/main/java/com/example/vrishankgupta/boilerplate/BeforeBounce.java
@@ -16,7 +16,7 @@ import com.wrlds.sdk.Ball;
 public class BeforeBounce extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
     static int count = 0;
 
-//    Ball a;
+    Ball a;
     ImageView img;
     public static final String API_KEY = "AIzaSyCE7i9MJ3YuDuqy9cn4YqgSf-qDESbgXh4";
     public static final String VIDEOID = "aA_mqSzbkM0";
@@ -26,7 +26,7 @@ public class BeforeBounce extends YouTubeBaseActivity implements YouTubePlayer.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_before);
         img = findViewById(R.id.img);
-//        a=MainActivity.a;
+        a=MainActivity.a;
         YouTubePlayerView youTubePlayerView = findViewById(R.id.ytPlayer);
         youTubePlayerView.initialize(API_KEY,this);
 
@@ -37,19 +37,19 @@ public class BeforeBounce extends YouTubeBaseActivity implements YouTubePlayer.O
 //                Toast.makeText(a, "Shaken"  + v, Toast.LENGTH_SHORT).show();
 //            }
 //        });
-//        a.setOnBounceListener(new Ball.OnBounceListener() {
-//            @Override
-//            public void onBounce(int i, float v) {
-//                Log.d("BounceType", Ball.BounceType.HARD + "");
-//                String str = String.valueOf(v);
-//                if(v > 10)
-//                {
-//                    count++;
-//                    startActivity(new Intent(BeforeBounce.this,AfterBounce.class).putExtra("force",str).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-//                    finish();
-//                }
-//            }
-//        });
+        a.setOnBounceListener(new Ball.OnBounceListener() {
+            @Override
+            public void onBounce(int i, float v) {
+                Log.d("BounceType", Ball.BounceType.HARD + "");
+                String str = String.valueOf(v);
+                if(v > 10)
+                {
+                    count++;
+                    startActivity(new Intent(BeforeBounce.this,AfterBounce.class).putExtra("force",str).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                    finish();
+                }
+            }
+        });
 
     }
 
